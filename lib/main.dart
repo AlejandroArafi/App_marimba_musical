8a4b868b4d5ffb22bde6a868214f0ae169f64a78
Alejandro <alejandro.arafi@gmail.com>
2024-07-11 09:38:55 -0400

    estructura app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Marimba Virtual'),
         ),
+        body: const Marimba(),
+        backgroundColor: Colors.pink[200],
       ),
+    );
+  }
+}
+
+class Marimba extends StatelessWidget {
+  const Marimba({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final player = AudioPlayer();
+    return Column(
+      children: List.generate(8, (index) {
+        return GestureDetector(
+          onTap: () {
+            player.play(AssetSource('sounds/marimba$index.mp3'));
+          },
+          child: Container(
+            margin: const EdgeInsets.all(5.0),
+            width: double.infinity,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: Colors.orange[(index + 1) * 100],
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ), // changes position of shadow
+              ],
+            ),
+            child: Center(
+              child: Text(
+                'Note ${index + 1}',
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
