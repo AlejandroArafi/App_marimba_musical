f4a85366057c475c46f5ac92aca44e7bbbe2f98e
Alejandro <alejandro.arafi@gmail.com>
2024-07-11 22:14:55 -0400

    audios ok

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,10 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Marimba Virtual'),
+          title: const Text('Marimba musical'),
         ),
         body: const Marimba(),
-        backgroundColor: Colors.pink[200],
+        backgroundColor: Colors.pink[100],
       ),
     );
   }
@@ -28,35 +28,66 @@ class Marimba extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final player = AudioPlayer();
+    final List<Color> noteColors = [
+      const Color(0xFFFF8A80),
+      const Color(0xFFFFD180),
+      const Color(0xFFFFF59D),
+      const Color(0xFFA5D6A7),
+      const Color(0xFF80CBC4),
+      const Color(0xFF90CAF9),
+      const Color(0xFFCE93D8),
+      const Color(0xFFF48FB1),
+    ];
+
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: List.generate(8, (index) {
         return GestureDetector(
           onTap: () {
-            player.play(AssetSource('sounds/marimba$index.mp3'));
+            final noteNumber = index + 1;
+            player.play(AssetSource('sounds/marimba$noteNumber.mp3'),
+                position: Duration.zero);
           },
-          child: Container(
-            margin: const EdgeInsets.all(5.0),
-            width: double.infinity,
-            height: 60.0,
-            decoration: BoxDecoration(
-              color: Colors.orange[(index + 1) * 100],
-              borderRadius: BorderRadius.circular(8.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ), // changes position of shadow
-              ],
-            ),
-            child: Center(
-              child: Text(
-                'Note ${index + 1}',
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold),
+          child: Row(
+            children: [
+              const Spacer(),
+              Container(
+                margin: const EdgeInsets.all(5.0),
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  color: noteColors[index],
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: InkWell(
+                  onTap: () {
+                    final noteNumber = index + 1;
+                    player.play(AssetSource('sounds/marimba$noteNumber.mp3'),
+                        position: Duration.zero);
+                  },
+                  splashColor: Colors.white.withOpacity(0.5),
+                  highlightColor: Colors.white.withOpacity(0.2),
+                  child: Center(
+                    child: Text(
+                      'Nota ${index + 1}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const Spacer()
+            ],
           ),
         );
       }),
